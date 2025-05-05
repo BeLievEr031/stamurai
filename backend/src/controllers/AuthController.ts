5d5b929bc53e44675978fcfd273e7a6fe3fe6bde
BeLieVer031 <sandyrajak031@gmail.com>
2025-05-05 15:23:26 +0530

    add user registration.

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,7 +1,31 @@
+import { NextFunction, Response } from "express";
 import { AuthService } from "../services";
+import { AuthRequest } from "../types";
+import { validationResult } from "express-validator";
+import { HttpStatus } from "../utils/constant";
 
 class AuthController {
     constructor(private authService: AuthService) { }
+
+    async register(req: AuthRequest, res: Response, next: NextFunction) {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                res.status(HttpStatus.UNPROCESSABLE_ENTITY).json({ errors: errors.array() });
+                return;
+            }
+
+            const registeredUser = await this.authService.register(req.body);
+
+            res.status(HttpStatus.CREATED).json({
+                message: "User registered successfully.",
+                user: registeredUser
+            })
+
+        } catch (error) {
+            next(error);
+        }
+    }
 }
 
 export default AuthController;
--- backend/src/services/AuthService.ts
+++ backend/src/services/AuthService.ts
@@ -1,7 +1,12 @@
 import { User } from "../models";
+import { IUser } from "../types";
 
 class AuthService {
     constructor(private userRepo: typeof User) { }
+
+    async register(user: IUser) {
+        return await this.userRepo.create(user);
+    }
 }
 
 export default AuthService;
