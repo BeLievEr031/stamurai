28faf317d90d5bfcf5896b41a0189f2fa9147767
BeLieVer031 <sandyrajak031@gmail.com>
2025-05-12 11:17:00 +0530

    resolve date-fns error

--- frontend/http/index.ts
+++ frontend/http/index.ts
@@ -1,11 +1,11 @@
 import axios from "axios"
 import { io } from "socket.io-client";
 const api = axios.create({
-    baseURL: `https://stamurai-il1u.onrender.com/api/v1`,
+    baseURL: `http://localhost:5000/api/v1`,
     withCredentials: true
 })
 
-const socket = io('https://stamurai-il1u.onrender.com', {
+const socket = io('http://localhost:5000', {
     autoConnect: true,
     transports: ['websocket'],
     withCredentials: true,
